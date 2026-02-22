029a6b06577ad233cbd2eec90a7b2ede56b21b6c
zhenia <zheniapravdenko@gmail.com>
2026-02-22 17:19:02 +0200

    change parallel logic and output

--- main.cpp
+++ main.cpp
@@ -31,45 +31,31 @@ Result solveSequential(const vector<int>& data) {
 
 mutex mtx;
 void solveMutexPart(const vector<int>& data, int start, int end, Result& globalRes) {
-    long long localSum = 0;
-    int localMin = INT_MAX;
-
     for (int i = start; i < end; ++i) {
         if (data[i] != 0 && data[i] % 13 == 0) {
-            localSum += data[i];
-            if (data[i] < localMin) localMin = data[i];
+            lock_guard<mutex> lock(mtx);
+            globalRes.sum += data[i];
+            if (data[i] < globalRes.min_val) globalRes.min_val = data[i];
         }
     }
-
-    lock_guard<mutex> lock(mtx);
-    globalRes.sum += localSum;
-    if (localMin < globalRes.min_val) globalRes.min_val = localMin;
 }
 
 void solveAtomicCASPart(const vector<int>& data, int start, int end, atomic<long long>& globalSum, atomic<int>& globalMin) {
-    long long localSum = 0;
-    int localMin = INT_MAX;
-
     for (int i = start; i < end; ++i) {
         if (data[i] != 0 && data[i] % 13 == 0) {
-            localSum += data[i];
-            if (data[i] < localMin) localMin = data[i];
+            int val = data[i];
+
+            long long oldSum;
+            do {
+                oldSum = globalSum.load();
+            } while (!globalSum.compare_exchange_weak(oldSum, oldSum + val));
+
+            int oldMin;
+            do {
+                oldMin = globalMin.load();
+                if (val >= oldMin) break; 
+            } while (!globalMin.compare_exchange_weak(oldMin, val));
         }
-    }
-
-    if (localSum != 0) {
-        long long oldSum;
-        do {
-            oldSum = globalSum.load();
-        } while (!globalSum.compare_exchange_weak(oldSum, oldSum + localSum));
-    }
-
-    if (localMin != INT_MAX) {
-        int oldMin;
-        do {
-            oldMin = globalMin.load();
-            if (localMin >= oldMin) break;
-        } while (!globalMin.compare_exchange_weak(oldMin, localMin));
     }
 }
 
@@ -80,12 +66,17 @@ void prepareData(vector<int>& data, size_t n) {
 }
 
 int main() {
-    vector<size_t> dimensions = { 10000, 100000, 1000000, 10000000, 100000000 };
-    vector<int> threadCounts = { 4, 8, 16, 32, 64, 128, 256 };
+    const int tc = 4;
+    const size_t start_n = 1000;      
+    const size_t end_n = 10000000;    
+    const size_t step = 500000;      
 
     cout << fixed << setprecision(3);
+    cout << "-----------------------------------------------------------------------" << endl;
+    cout << setw(12) << "N" << " | " << setw(12) << "Seq (ms)" << " | " << setw(12) << "Mutex (ms)" << " | " << setw(12) << "CAS (ms)" << endl;
+    cout << "-------------|--------------|--------------|--------------" << endl;
 
-    for (size_t n : dimensions) {
+    for (size_t n = start_n; n <= end_n; n += step) { 
         vector<int> data(n);
         prepareData(data, n);
 
@@ -94,52 +85,46 @@ int main() {
         auto s2 = chrono::high_resolution_clock::now();
         double timeSeq = chrono::duration<double, milli>(s2 - s1).count();
 
-        cout << "\n>>> N = " << n << " | Sequential: " << timeSeq << " ms" << endl;
-        cout << setw(10) << "Threads" << " | " << setw(15) << "Mutex (ms)" << " | " << setw(15) << "CAS (ms)" << endl;
-        cout << "-----------|-----------------|-----------------" << endl;
+        Result resMtx;
+        vector<thread> threads;
+        int chunkSize = n / tc;
 
-        for (int tc : threadCounts) {
-
-            Result resMtx;
-            vector<thread> threads;
-            int chunkSize = n / tc;
-
-            auto m1 = chrono::high_resolution_clock::now();
-            for (int i = 0; i < tc; ++i) {
-                int start = i * chunkSize;
-                int end = (i == tc - 1) ? (int)n : (i + 1) * chunkSize;
-                threads.emplace_back(solveMutexPart, ref(data), start, end, ref(resMtx));
-            }
-            for (auto& t : threads) t.join();
-            auto m2 = chrono::high_resolution_clock::now();
-            double timeMtx = chrono::duration<double, milli>(m2 - m1).count();
-
-            atomic<long long> atomicSum(0);
-            atomic<int> atomicMin(INT_MAX);
-            threads.clear();
-
-            auto a1 = chrono::high_resolution_clock::now();
-            for (int i = 0; i < tc; ++i) {
-                int start = i * chunkSize;
-                int end = (i == tc - 1) ? (int)n : (i + 1) * chunkSize;
-                threads.emplace_back(solveAtomicCASPart, ref(data), start, end, ref(atomicSum), ref(atomicMin));
-            }
-            for (auto& t : threads) t.join();
-            auto a2 = chrono::high_resolution_clock::now();
-            double timeCAS = chrono::duration<double, milli>(a2 - a1).count();
-
-            cout << setw(10) << tc << " | " 
-                 << setw(15) << timeMtx << " | " 
-                 << setw(15) << timeCAS << endl;
-
-            antiOptimization += resSeq.sum + resMtx.sum + atomicSum.load();
+        auto m1 = chrono::high_resolution_clock::now();
+        for (int i = 0; i < tc; ++i) {
+            int start = (int)(i * chunkSize);
+            int end = (i == tc - 1) ? (int)n : (int)((i + 1) * chunkSize);
+            threads.emplace_back(solveMutexPart, ref(data), start, end, ref(resMtx));
         }
-        cout << "-----------------------------------------------" << endl;
+        for (auto& t : threads) t.join();
+        auto m2 = chrono::high_resolution_clock::now();
+        double timeMtx = chrono::duration<double, milli>(m2 - m1).count();
+
+        atomic<long long> atomicSum(0);
+        atomic<int> atomicMin(INT_MAX);
+        threads.clear();
+
+        auto a1 = chrono::high_resolution_clock::now();
+        for (int i = 0; i < tc; ++i) {
+            int start = (int)(i * chunkSize);
+            int end = (i == tc - 1) ? (int)n : (i + 1) * chunkSize;
+            threads.emplace_back(solveAtomicCASPart, ref(data), start, end, ref(atomicSum), ref(atomicMin));
+        }
+        for (auto& t : threads) t.join();
+        auto a2 = chrono::high_resolution_clock::now();
+        double timeCAS = chrono::duration<double, milli>(a2 - a1).count();
+
+        cout << setw(12) << n << " | " 
+             << setw(12) << timeSeq << " | " 
+             << setw(12) << timeMtx << " | " 
+             << setw(12) << timeCAS << endl;
+
+        antiOptimization += resSeq.sum + resMtx.sum + atomicSum.load();
         
         data.clear();
         data.shrink_to_fit();
     }
 
-    cout << "\nCheck sum: " << antiOptimization << endl;
+    cout << "-----------------------------------------------------------------------" << endl;
+    cout << "Check sum: " << antiOptimization << endl;
     return 0;
 }
