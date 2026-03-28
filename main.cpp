486f2777a40fb8a36a5b8518868425692846dc8e
zhenia <zheniapravdenko@gmail.com>
2026-03-28 14:57:18 +0200

    init basic thread pool with worker threads

--- main.cpp
+++ main.cpp
@@ -1,79 +1,94 @@
 #include <iostream>
 #include <vector>
-#include <random>
+#include <queue>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 #include <chrono>
-#include <iomanip>
+#include <atomic>
+#include <string>
 
 using namespace std;
 
-void solvePart(vector<double>& matrix, int n, int startRow, int endRow) {
-    for (int i = startRow; i < endRow; ++i) {
-        double rowProduct = 1.0;
-        for (int j = 0; j < n; ++j) {
-            rowProduct *= matrix[i * (size_t)n + j];
+struct Task {
+    int id;
+    chrono::steady_clock::time_point creation_time;
+    int random_delay; 
+
+    Task(int _id, int _delay) 
+        : id(_id), creation_time(chrono::steady_clock::now()), random_delay(_delay) {}
+};
+
+class ThreadPool {
+private:
+    vector<thread> workers;
+    queue<Task> execution_queue;
+    mutex queue_mtx;
+    mutex cout_mtx;
+    condition_variable cv_workers;
+    atomic<bool> is_stopped{false};
+
+    void safe_print(const string& msg) {
+        lock_guard<mutex> lock(cout_mtx);
+        cout << msg << endl;
+    }
+
+    void worker_loop(int thread_id) {
+        while (true) {
+            Task* task = nullptr;
+            {
+                unique_lock<mutex> lock(queue_mtx);
+                cv_workers.wait(lock, [this] {
+                    return is_stopped || !execution_queue.empty();
+                });
+
+                if (is_stopped && execution_queue.empty()) return;
+
+                if (!execution_queue.empty()) {
+                    task = new Task(execution_queue.front());
+                    execution_queue.pop();
+                }
+            }
+
+            if (task) {
+                safe_print("[Worker " + to_string(thread_id) + "] started Task ID: " + to_string(task->id));
+                this_thread::sleep_for(chrono::seconds(task->random_delay));
+                safe_print("[Worker " + to_string(thread_id) + "] completed Task ID: " + to_string(task->id));
+                delete task;
+            }
         }
-        matrix[i * (size_t)n + (n - 1 - i)] = rowProduct;
-    }
-}
-
-double runParallel(vector<double>& matrix, int n, int numThreads) {
-    vector<thread> threads;
-    threads.reserve(numThreads);
-
-    int rowsPerThread = n / numThreads;
-
-    auto start = chrono::high_resolution_clock::now();
-
-    for (int i = 0; i < numThreads; ++i) {
-        int startRow = i * rowsPerThread;
-        int endRow = (i == numThreads - 1) ? n : (i + 1) * rowsPerThread;
-
-        threads.emplace_back(solvePart, ref(matrix), n, startRow, endRow);
     }
 
-    for (auto& t : threads) { t.join(); }
-
-    auto end = chrono::high_resolution_clock::now();
-    return chrono::duration<double, milli>(end - start).count();
-}
-
-void prepareData(vector<double>& matrix, int n) {
-    mt19937 gen(42);
-    uniform_real_distribution<> dis(1.0, 3.0);
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            matrix[(size_t)i * n + j] = (j == n - 1 - i) ? 1.0 : dis(gen);
+public:
+    ThreadPool(int threads_count = 6) {
+        for (int i = 0; i < threads_count; ++i) {
+            workers.emplace_back(&ThreadPool::worker_loop, this, i + 1);
         }
     }
-}
+
+    void enqueue(Task task) {
+        {
+            lock_guard<mutex> lock(queue_mtx);
+            execution_queue.push(task);
+        }
+        cv_workers.notify_one();
+    }
+
+    ~ThreadPool() {
+        is_stopped = true;
+        cv_workers.notify_all();
+        for (auto& w : workers) {
+            if (w.joinable()) w.join();
+        }
+    }
+};
 
 int main() {
-    vector<int> dimensions = { 100, 1000, 5000, 10000, 20000 };
-    vector<int> threadCounts = { 1, 4, 8, 16, 32, 64, 128, 256 };
-
-    cout << fixed << setprecision(4);
-    cout << setw(10) << "N" << " | " << setw(8) << "Threads" << " | " << "Time (ms)" << endl;
-    cout << "-----------|----------|------------" << endl;
-
-    for (int n : dimensions) {
-        vector<double> matrix((size_t)n * n);
-        prepareData(matrix, n);
-
-        for (int tc : threadCounts) {
-            runParallel(matrix, n, tc);
-
-            double time = runParallel(matrix, n, tc);
-
-            cout << setw(10) << n << " | " << setw(8) << tc << " | " << time << " ms" << endl;
-            
-            volatile double checksum = matrix[0];
-        }
-        cout << "-----------|----------|------------" << endl;
-        
-        matrix.clear();
-        matrix.shrink_to_fit();
+    ThreadPool pool(6);
+    for (int i = 1; i <= 10; ++i) {
+        pool.enqueue(Task(i, 2));
+        this_thread::sleep_for(chrono::milliseconds(500));
     }
-
+    this_thread::sleep_for(chrono::seconds(10));
     return 0;
 }
