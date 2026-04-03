0377119a457ef14572a04fa05f2cc426e5ae4643
zhenia <zheniapravdenko@gmail.com>
2026-04-03 17:08:22 +0300

    implement multithreading and custom application protocol

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
 #include <winsock2.h>
-#include <string>
+#include <vector>
+#include <random>
 
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
 
-const char* IP = "127.0.0.1";
-const int PORT = 8080;
+const char* SERVER_IP = "127.0.0.1";
+const int SERVER_PORT = 8080;
+const int MATRIX_SIZE = 5; 
+
+uint64_t packDouble(double d) {
+    uint64_t res; memcpy(&res, &d, sizeof(double)); return htonll(res); 
+}
 
 int main() {
-    WSADATA wsa;
-    WSAStartup(MAKEWORD(2, 2), &wsa);
-
+    WSADATA wsa; WSAStartup(MAKEWORD(2, 2), &wsa);
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr = { AF_INET, htons(SERVER_PORT) };
+    addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-    sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = inet_addr(IP);
+    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) != 0) return 1;
 
-    cout << "Attempting to connect to " << IP << ":" << PORT << "..." << endl;
+    send(sock, "CONFIG    ", 10, 0);
+    uint32_t conf[2] = { (uint32_t)htonl(MATRIX_SIZE), (uint32_t)htonl(1) };
+    send(sock, (char*)conf, sizeof(conf), 0);
+    char buf[11] = {0};
+    recv(sock, buf, 10, 0);
+    cout << "Config: " << buf << endl;
 
-    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) == 0) {
-        cout << "Connected!" << endl;
-
-        send(sock, "HELLO     ", 10, 0);
-
-        char buf[11] = {0};
-        recv(sock, buf, 10, 0);
-        cout << "Server replied: " << buf << endl;
-    } else {
-        cout << "Connection failed." << endl;
+    send(sock, "DATA      ", 10, 0);
+    for(int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; ++i) {
+        uint64_t nv = packDouble(1.5 + i); 
+        send(sock, (char*)&nv, sizeof(nv), 0);
     }
+    recv(sock, buf, 10, 0);
+    cout << "Data: " << buf << endl;
+
+    send(sock, "START     ", 10, 0);
+    recv(sock, buf, 10, 0);
+    cout << "Computation: " << buf << endl;
 
     closesocket(sock);
     WSACleanup();
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,41 +1,104 @@
 #include <iostream>
 #include <winsock2.h>
+#include <vector>
 #include <string>
+#include <thread>
+#include <map>
+#include <mutex>
 
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
 
-const int PORT = 8080;
+const int SERVER_PORT = 8080;
+
+struct ClientData {
+    int matrixSize = 0;
+    int threadCount = 1;
+    vector<double> matrix;
+    bool isDone = false;
+};
+
+map<SOCKET, ClientData> clients;
+mutex clientsMutex;
+
+uint64_t packDouble(double d) {
+    uint64_t res; 
+    memcpy(&res, &d, sizeof(double)); 
+    return htonll(res); 
+}
+
+double unpackDouble(uint64_t net_int) {
+    uint64_t host_int = ntohll(net_int);
+    double res; 
+    memcpy(&res, &host_int, sizeof(double)); 
+    return res;
+}
+
+bool recv_all(SOCKET s, char* buf, int len) {
+    int total = 0;
+    while (total < len) {
+        int n = recv(s, buf + total, len - total, 0);
+        if (n <= 0) return false;
+        total += n;
+    }
+    return true;
+}
+
+void solvePart(vector<double>* matrix, int n, int startRow, int endRow) {
+    for (int i = startRow; i < endRow; ++i) {
+        double rowProduct = 1.0;
+        for (int j = 0; j < n; ++j) {
+            if (j != (n - 1 - i)) rowProduct *= (*matrix)[(size_t)i * n + j];
+        }
+        (*matrix)[(size_t)i * n + (n - 1 - i)] = rowProduct;
+    }
+}
+
+void handleClient(SOCKET clientSocket) {
+    char cmdBuf[11] = {0};
+    while (true) {
+        if (!recv_all(clientSocket, cmdBuf, 10)) break;
+        string cmd(cmdBuf);
+
+        if (cmd.find("CONFIG") != string::npos) {
+            uint32_t config[2];
+            if (recv_all(clientSocket, (char*)config, sizeof(config))) {
+                lock_guard<mutex> lock(clientsMutex);
+                clients[clientSocket].matrixSize = ntohl(config[0]);
+                clients[clientSocket].threadCount = ntohl(config[1]);
+                send(clientSocket, "CONFIG_OK ", 10, 0);
+            }
+        }
+        else if (cmd.find("DATA") != string::npos) {
+            int n = clients[clientSocket].matrixSize;
+            clients[clientSocket].matrix.resize((size_t)n * n);
+            for (int i = 0; i < n * n; ++i) {
+                uint64_t netVal;
+                recv_all(clientSocket, (char*)&netVal, sizeof(netVal));
+                clients[clientSocket].matrix[i] = unpackDouble(netVal);
+            }
+            send(clientSocket, "DATA_OK   ", 10, 0);
+        }
+        else if (cmd.find("START") != string::npos) {
+            int n = clients[clientSocket].matrixSize;
+            solvePart(&clients[clientSocket].matrix, n, 0, n); 
+            clients[clientSocket].isDone = true;
+            send(clientSocket, "STARTED   ", 10, 0);
+        }
+    }
+    closesocket(clientSocket);
+}
 
 int main() {
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
-
+    WSADATA wsa; WSAStartup(MAKEWORD(2, 2), &wsa);
     SOCKET srv = socket(AF_INET, SOCK_STREAM, 0);
-    
-    sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    sockaddr_in a = { AF_INET, htons(SERVER_PORT), INADDR_ANY };
+    bind(srv, (sockaddr*)&a, sizeof(a));
+    listen(srv, SOMAXCONN);
 
-    bind(srv, (sockaddr*)&addr, sizeof(addr));
-    listen(srv, 1);
-
-    cout << "Server started on port " << PORT << ". Waiting for client..." << endl;
-
-    SOCKET cl = accept(srv, nullptr, nullptr);
-    if (cl != INVALID_SOCKET) {
-        cout << "Client connected!" << endl;
-        
-        char buffer[1024] = {0};
-        recv(cl, buffer, 1024, 0);
-        cout << "Received from client: " << buffer << endl;
-
-        send(cl, "HELLO_OK  ", 10, 0);
+    cout << "Server multithreaded started..." << endl;
+    while (true) {
+        SOCKET cl = accept(srv, nullptr, nullptr);
+        if (cl != INVALID_SOCKET) thread(handleClient, cl).detach();
     }
-
-    closesocket(cl);
-    closesocket(srv);
-    WSACleanup();
-    return 0;
 }
